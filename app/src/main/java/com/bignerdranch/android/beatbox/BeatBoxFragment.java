d188e9b90b4a1b9f0cb02b1016e25ab9ba74f88d
GarryMcKee <gmk0232@gmail.com>
2017-07-31 17:35:26 +0100

    Add SoundHolder class for RecyclerView

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
@@ -1,7 +1,18 @@
 package com.bignerdranch.android.beatbox;
 
+import android.app.LauncherActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bignerdranch.android.beatbox.databinding.FragmentBeatBoxBinding;
+import com.bignerdranch.android.beatbox.databinding.ListItemSoundBinding;
 
 /**
  * Created by Garry on 14/07/2017.
@@ -16,5 +27,24 @@ public class BeatBoxFragment extends Fragment {
         BeatBoxFragment fragment = new BeatBoxFragment();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        FragmentBeatBoxBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_beat_box, container, false);
+
+        //Set a 3 column GridLayoutManager for the recycler view
+        binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        return binding.getRoot();
+    }
+
+    private class SoundHolder extends RecyclerView.ViewHolder {
+        private ListItemSoundBinding mBinding;
+
+        private SoundHolder(ListItemSoundBinding binding) {
+            super(binding.getRoot());
+            mBinding = binding;
+        }
     }
 }
